e60a19f5a7f11f6fec45fb2c252aeda4bce273ad
Nilinofficial <itsmenilin18@gmail.com>
2026-07-21 12:29:28 +0530

    Fix status bar background color issue

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,17 +3,24 @@ import Header from "@/components/custom/Header";
 import Streak from "@/components/custom/Streak";
 import { ThemedView } from "@/components/themed-view";
 import ScreenContent  from "@/components/wrappers/ScreenWrapper";
+import { StatusBar } from "expo-status-bar";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { useColorScheme } from "react-native";
+
 export default function HomeScreen() {
+
+  const colorScheme = useColorScheme();
+
   return (
-    <ThemedView style={{ flex: 1 }}>
+
       <SafeAreaView style={{ flex: 1 }}>
+        <StatusBar style={colorScheme === "dark" ? "light" : "dark"} />
         <ScreenContent>
           <Header />
           <Streak />
         </ScreenContent>
 
       </SafeAreaView>
-    </ThemedView>
+
   );
 }
